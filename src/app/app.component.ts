6cca32ff75b7cee29f420ff42d7b20580909b74f
Arkadiusz Aptewicz <Arkadiusz.Aptewicz@dbschenker.com>
2021-01-10 10:09:55 +0100

    clean up and changing background color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'sda APP!!!';
   imgSrc = 'https://angular.io/assets/images/logos/angular/logo-nav@2x.png';
   linkUrl = 'https://google.com';
   headerTextColor: 'yellow' | 'blue' = 'yellow';
+  backgroundColor: 'red' | 'black' = 'red';
 
-  // TODO:
-  // introduce new field with proper visibility witch can hold values 'red' and 'black'
-  // this value should change every 1000 ms
-  // this value should be responsible for background color of header
+  private interval: any | null = null;
 
   ngOnInit(): void {
-    setInterval(() => {
-      if (this.headerTextColor === 'yellow') {
+    this.interval = setInterval(() => {
+      if (this.headerTextColor === 'yellow' && this.backgroundColor === 'red') {
         this.headerTextColor = 'blue';
+        this.backgroundColor = 'black';
       } else {
         this.headerTextColor = 'yellow';
+        this.backgroundColor = 'red';
       }
     }, 1000);
+  }
+
+  ngOnDestroy(): void {
+    if (this.interval != null) {
+      clearInterval(this.interval);
+    }
   }
 
   clickHandler() {
