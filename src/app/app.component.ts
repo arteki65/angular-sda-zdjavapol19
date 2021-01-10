830ffa3981d2baf330260d934ecea92a7977a5e1
Arkadiusz Aptewicz <Arkadiusz.Aptewicz@dbschenker.com>
2021-01-10 09:52:53 +0100

    one way databing todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,28 @@ export class AppComponent implements OnInit {
   title = 'sda APP!!!';
   imgSrc = 'https://angular.io/assets/images/logos/angular/logo-nav@2x.png';
   linkUrl = 'https://google.com';
+  headerTextColor: 'yellow' | 'blue' = 'yellow';
 
-  ngOnInit(): void {}
+  // TODO:
+  // introduce new field with proper visibility witch can hold values 'red' and 'black'
+  // this value should change every 1000 ms
+  // this value should be responsible for background color of header
+
+  ngOnInit(): void {
+    setInterval(() => {
+      if (this.headerTextColor === 'yellow') {
+        this.headerTextColor = 'blue';
+      } else {
+        this.headerTextColor = 'yellow';
+      }
+    }, 1000);
+  }
+
+  clickHandler() {
+    console.log('button was clicked');
+  }
+
+  onMouseMove() {
+    console.log('te mouse was moved');
+  }
 }
