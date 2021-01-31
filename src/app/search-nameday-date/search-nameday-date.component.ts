d0bad85295d4c9ed774b5fec1434d79c4b272ee3
wojciech184r <wrajczakowski@yahoo.co.uk>
2021-01-31 11:27:26 +0000

    Full Working App

--- src/app/search-nameday-date/search-nameday-date.component.ts
+++ src/app/search-nameday-date/search-nameday-date.component.ts
@@ -13,25 +13,10 @@ export class SearchNamedayDateComponent implements OnInit, OnDestroy {
   name: string = '';
   showContent = true;
   date:any='';
-  dates:any = '';
-  month = '';
-  day ='';
-  showValue:any ='';
-  
-  
-  
-  
-
-  private subscription: Subscription | null = null;
-  
-  
-  
+  showValue:{month:number; day:number;}[]=[];
   constructor(private api: NamedaysApiService, private uiService: UiService) {}
   
   ngOnDestroy(): void {
-    if (this.subscription != null) {
-      this.subscription.unsubscribe();
-    }
   }
 
   ngOnInit(): void {
@@ -41,54 +26,14 @@ export class SearchNamedayDateComponent implements OnInit, OnDestroy {
   }
 
   onSearch() {
-    /* to jeden pomysł jaki przyszedł mi do gowy ale niestety nie pokazuje rezultatu */
-    
-    /*if (this.name === '') {
-      return;
-    }
-    this.uiService.isLoading.next(true);
-    this.api.searchByName(this.name).subscribe((result) => { 
-      
-    let obj = {result};
-    const showValue = Object.getOwnPropertyNames(obj).forEach(key => {
-    let variable = [key]}); 
-    this.uiService.isLoading.next(false);
-      */
-     
-    /*następny pomysł, który pokazuje ile znaleziono odpowiedzi na zapytanie (ale nie o to nam chodzi ):*/
-    /*
-    onSearch() {
     if (this.name === '') {
       return;
     }
     this.uiService.isLoading.next(true);
-    this.api.searchByName(this.name).subscribe((getdate) => { 
-    const showValue = console.log(getdate.date.length);
-    this.uiService.isLoading.next(false);
-     */
-
-     /*kolejny pomysł jest następujący i najbardziej rozsądny ale równie nici */
-
-     /*
-     if (this.name === '') {
-      return;
-    }
-    this.uiService.isLoading.next(true);
-    this.api.searchByName(this.name).subscribe((getdate) => { 
-    const showValue = console.log(getdate.date.map(x=>(x.day,x.month)));
-    this.uiService.isLoading.next(false);
-     */
-    
-    /*
-    if (this.name === '') {
-      return;
-    }
-    this.uiService.isLoading.next(true);
+   
     this.api.searchByName(this.name).subscribe((result) => { 
-    const showValue = result.dates.filter(result=>(result.day,result.month))
+    this.showValue = result.dates; 
     this.uiService.isLoading.next(false);
-     }) });
-     */
-    
+     }) ;
   }
 }
